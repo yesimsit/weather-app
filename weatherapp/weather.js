1a0f02455e4bde65fc0c854b0a04bb931b9b5dac
yesimsit <88620775+yesimsit@users.noreply.github.com>
2022-11-01 15:11:58 +0300

    Add files via upload

---
+++ weatherapp/weather.js
@@ -0,0 +1,51 @@
+let weather={
+    "apiKey": "52ab776c58035c6dc4637c06f955f137",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + city 
+        + "&units=metric&appid="
+        + this.apiKey
+        )
+        .then((res)=> res.json() 
+         )
+        .then((data)=> this.showWeather(data))
+        
+
+    },
+    showWeather: function(data){
+        const {name} = data;
+        const { icon, description } = data.weather[0];
+        const {temp, humidity}= data.main;
+        const { speed} = data.wind;
+      console.log(name,icon,description,temp,humidity,speed)
+      document.querySelector(".city").innerText= "Weather in " + name
+      document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText= description;
+      document.querySelector(".temp").innerText= temp + "°C";
+      document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+      document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      document.querySelector(".weather").classList.remove("loading");
+    },
+search: function () {
+    this.fetchWeather(document.querySelector(".search-input").value);
+  },
+   
+    };
+    
+    document.querySelector(".btn").addEventListener("click", function () {
+      weather.search();
+    });
+    document
+  .querySelector(".search-input")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+  weather.fetchWeather("Ardahan")
